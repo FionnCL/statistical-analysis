5f4d18048c8bfcc62996b11b7fcaebd4e06ba941
FionnCL <103070351+FionnCL@users.noreply.github.com>
2023-05-24 23:10:16 +0100

    Finalized API v1

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 import NumberOfSamples as NoS
 import LinearRegressionModel as LRM
@@ -10,7 +10,7 @@ import SampleSize as SS
 
 # creates api app
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+# CORS(app, origins=["http://localhost:3000"])
 api = Api(app)
 
 # Sampling
@@ -169,4 +169,4 @@ api.add_resource(sampleSize, '/estimation/sample-size')
 
 # runs program if running from this file
 if __name__ == '__main__':
-    app.run(port=4000, debug=True)
+    app.run()
