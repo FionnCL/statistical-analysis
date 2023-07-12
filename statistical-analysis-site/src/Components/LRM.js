f2c87120504724a65aa26fcb394dd9ae9b5476d4
FionnCL <103070351+FionnCL@users.noreply.github.com>
2023-07-12 13:34:01 +0100

    LRM Results Working

--- statistical-analysis-site/src/Components/LRM.js
+++ statistical-analysis-site/src/Components/LRM.js
@@ -18,6 +18,7 @@ export default function LRM(){
     ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
     let [explanatory, setExplanatory] = useState("");
     let [response, setResponse] = useState("");
+    let [results, setResults] = useState(<div></div>);
 
     const testX = [0.56,0.23,1.56,0.07,0.13,1.72,0.46,1.27,0.69,0.45,1.22,0.36,0.40,0.11,0.56];
     const testY = [2.18,-0.66,0.21,-2.51,-2.63,1.27,-0.17,0.78,0.02,-0.63,0.07,0.46,-0.04,-3.57,1.63];
@@ -40,8 +41,9 @@ export default function LRM(){
         };
         
     let getResults = ((results) => {
-        console.log(results);
-        return(
+        if(results != null ){
+            setResults
+            (
             <div className="container results">
                 <p>x̄:&nbsp;{results.xBar}</p>
                 <p>ȳ:&nbsp;{results.yBar}</p>
@@ -56,42 +58,27 @@ export default function LRM(){
                 <p>Sample Variance(Y):&nbsp;{results.sampleVarianceY}</p>
             </div>
         );
+        } else {
+            setResults(<div className="container results">Input points to see results!</div>);
+        }
     });
-
-    let results = {
-        "xBar": 0,
-        "yBar": -0.24,
-        "beta": 1.7021,
-        "alpha": -1.3464,
-        "correlation": 0.486,
-        "populationCovariance": 0.4536,
-        "sampleCovariance": 0.486,
-        "populationVarianceX": 0.2665,
-        "populationVarianceY": 2.3905,
-        "sampleVarianceX": 0.2855,
-        "sampleVarianceY": 2.5612
-    }
 
     async function getInfo(endpoint, data){
         if(!stringIsValid(explanatory) && !stringIsValid(response)) { return "Invalid string."; }
 
-        console.log(data);
-
-        let res = await fetch(('http://fionncl.pythonanywhere.com' + endpoint), {
+        await fetch(('http://fionncl.pythonanywhere.com' + endpoint), {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data)
+        }).then((res) => {
+            if(res.ok) {
+                res.json().then((json) => {
+                    getResults(json);
+                })
+            }
         });
-        getResults(res.json());
-        // .then((res) => {
-        //     console.log(res);
-        // }).then((res) => {
-        //     results = res;
-        //     getResults(results);
-        //     console.log(results);
-        // });
     }
 
     function parseData(){
@@ -111,7 +98,6 @@ export default function LRM(){
     // Perhaps maybe doesn't work.
     function stringIsValid(string) {
         const list = string.split(",");
-        console.log(list);
         for(let i = 0; i < list.length; i++){
             if(isNaN(parseFloat(list[i]))) { return false }
         }
@@ -144,7 +130,7 @@ export default function LRM(){
                     Submit
                 </button>
             </div>
-            {getResults(results)}
+            {results}
             <Scatter data={data}/>
         </div>
     );
